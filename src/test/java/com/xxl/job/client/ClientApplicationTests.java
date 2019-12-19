cce4df0cb0ded4c2ec34bb9c24118d8397e683d8
luojunjie <luojunjie9494@163.com>
2019-12-19 21:16:59 +0800

    init project

---
+++ src/test/java/com/xxl/job/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxl.job.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
